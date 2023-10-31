d80ef98218bf1e8fa4d08dd19ca5ffd53e8a59f7
Amakesaha <amakesasha@gmail.com>
2023-10-31 20:02:14 +0500

    2

--- src/struct/gui/button.rs
+++ src/struct/gui/button.rs
@@ -3,11 +3,27 @@ use crate::*;
 #[derive(Debug, Clone)]
 pub struct GuiButtonS {
 	pub(crate) name: String,
-	pub(crate) position: AabbS,
+	pub(crate) aabb: AabbS,
 	pub(crate) texture: TextureT,
 }
 
 impl Create for GuiButtonS {
 	type Output = GuiButtonS;
-	type Facts = ();
+	type Facts = (Stringl, [[f64; 2]; 2], TextureT);
+
+	fn new(facts: Self::Facts) -> Self::Output {
+		GuiButtonS {
+			name: facts.0,
+			aabb: AabbS::new(facts.1),
+			texture: facts.2,
+		}
+	}
+
+	fn default() -> Self::Output {
+		GuiButtonS {
+			name: "".to_string(),
+			aabb: AabbS::default(),
+			texture: Err(None),
+		}
+	}
 }
